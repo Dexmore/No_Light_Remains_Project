a628aeadc14a05815074593699d1b9e44f775c5b
r2p3 <106571349+deco12000@users.noreply.github.com>
2025-11-05 17:28:57 +0900

    KJH v0.0.51

--- Assets/3. Scripts/KJH/Interactable/DropItem.cs
+++ Assets/3. Scripts/KJH/Interactable/DropItem.cs
@@ -3,6 +3,8 @@ public class DropItem : Interactable
 {
     public override Type type => Type.DropItem;
     public bool isAuto = true;
+    public ItemData itemData;
+    public int money;
     public void Get()
     {
         
